1bf5e74095407ecb2c16bca126d8f6c158a8fe7e
Kevin Hill <kevin.hill@fortunabmc.com>
2023-08-09 06:35:34 -0700

    trying to get this working

---
+++ index.ts
@@ -0,0 +1,9 @@
+import { toAIFunction } from "@typeai/core"
+
+/** @description Given `text`, returns a number between 1 (positive) and -1 (negative) indicating its sentiment score. */
+function sentimentSpec(text: string): number | void {}
+const sentiment = toAIFunction(sentimentSpec)
+
+const score = await sentiment('That was surprisingly easy!')
+
+console.log(score);
